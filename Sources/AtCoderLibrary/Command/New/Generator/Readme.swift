cfa2e08c075c10f258360005aeea659a8e6e5d4e
Shota Kashihara <mercury4th@gmail.com>
2021-02-06 20:51:01 +0900

    Display decimal point

--- Sources/AtCoderLibrary/Command/New/Generator/Readme.swift
+++ Sources/AtCoderLibrary/Command/New/Generator/Readme.swift
@@ -13,7 +13,7 @@ struct Readme: Generator {
         問題名 | 実行時間制限 | メモリ制限
         :-- | --: | --:
         \(problems.map { problem in """
-        [\(problem.context.alphabet) \(problem.name)](\(problem.url)) | \(problem.timeLimit/1000) sec | \(problem.memoryLimit) MB
+        [\(problem.context.alphabet) \(problem.name)](\(problem.url)) | \(Double(problem.timeLimit)/1000) sec | \(problem.memoryLimit) MB
         """}.joined(separator: "\n"))
 
         """
