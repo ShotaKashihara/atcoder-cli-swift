2df6bc5e60958e786ccc97de7481d05ce100fc1e
Shota Kashihara <mercury4th@gmail.com>
2021-07-31 23:31:38 +0900

    Bump version 1.0.14

--- Sources/AtCoderCLI/main.swift
+++ Sources/AtCoderCLI/main.swift
@@ -5,7 +5,7 @@ import SwiftShell
 struct Command: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "AtCoder CLI for Swift.",
-        version: "1.0.13",
+        version: "1.0.14",
         subcommands: [
             New.self,
             Submit.self,
