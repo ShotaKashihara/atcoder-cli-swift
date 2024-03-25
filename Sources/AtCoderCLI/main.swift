a355669cc3d70bc5d48fd62491733435e9035adb
Shota Kashihara <mercury4th@gmail.com>
2024-03-25 09:41:59 +0900

    Update the version to 1.0.16

--- Sources/AtCoderCLI/main.swift
+++ Sources/AtCoderCLI/main.swift
@@ -5,7 +5,7 @@ import SwiftShell
 struct Command: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "AtCoder CLI for Swift.",
-        version: "1.0.15",
+        version: "1.0.16",
         subcommands: [
             New.self,
             Submit.self,
